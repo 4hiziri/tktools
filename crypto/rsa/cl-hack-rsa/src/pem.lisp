b0278011ff5e2de47a7a943b34c85664945bb2f1
tkgsy <meirvg@gmail.com>
2018-09-26 13:08:41 +0900

    Add description and refactor

--- crypto/rsa/cl-hack-rsa/src/pem.lisp
+++ crypto/rsa/cl-hack-rsa/src/pem.lisp
@@ -4,6 +4,7 @@
 
 @export
 (defstruct rsa-key
+  "RSA key pem fields"
   (version nil)
   (n nil)
   (e nil)
@@ -15,44 +16,62 @@
   (c nil) ;; coefficient: q^-1 mod p
   )
 
-(defun list2rsa-key (list)
-  (cond ((= (length list) 9) ;; private-key
-	 (make-rsa-key :version (nth 0 list)
-		       :n (nth 1 list)
-		       :e (nth 2 list)
-		       :d (nth 3 list)
-		       :p (nth 4 list)
-		       :q (nth 5 list)
-		       :e1 (nth 6 list)
-		       :e2 (nth 7 list)
-		       :c (nth 8 list)))
-	((= (length list) 2) ;; public-key
-	 (make-rsa-key :n (nth 0 list)
-		       :e (nth 1 list)))))
+(defun asp1-rsa-private (asp1-obj)
+  "convert ASP.1 object of RSA private key pem to RSA key"
+  (let ((vals (asp1-object-value asp1-obj)))
+    (make-rsa-key :version (asp1-object-value (nth 0 vals))
+		  :n (asp1-object-value (nth 1 vals))
+		  :e (asp1-object-value (nth 2 vals))
+		  :d (asp1-object-value (nth 3 vals))
+		  :p (asp1-object-value (nth 4 vals))
+		  :q (asp1-object-value (nth 5 vals))
+		  :e1 (asp1-object-value (nth 6 vals))
+		  :e2 (asp1-object-value (nth 7 vals))
+		  :c (asp1-object-value (nth 8 vals)))))
+
+(defun asp1-rsa-public (asp1-obj)
+  "Make RSA-KEY from asp1-obj from public-key's pem."
+  ;; RSA public key pem is represented by (object-id bit-string)
+  ;; object-id is just id, bit-string is real public-key value
+  ;; but first byte is pading-bit-length, so just do cdr
+  ;; rest part is DER format public key data
+  (let ((pubkey-data (asp1-object-value
+		      (car (parse-der
+			    (cdr (asp1-object-value
+				  (nth 1 (asp1-object-value asp1-obj)))))))))
+    ;; public-key pem only contains N and e.
+    (make-rsa-key :n (asp1-object-value (nth 0 pubkey-data))
+		  :e (asp1-object-value (nth 1 pubkey-data)))))
 
 ;; TODO: to util
 (defun read-string (stream)
+  "Return all content in stream as String"
   (do ((ch (read-char stream nil nil) (read-char stream nil nil))
        (ch-list nil (cons ch ch-list)))
       ((null ch)
        (coerce (reverse ch-list) 'string))))
 
 (defun trim-left-subseq (prefix sequence)
+  "Trim `prefix` from left"
   (if (alexandria:starts-with-subseq prefix sequence)
       (subseq sequence (length prefix))
       sequence))
 
 (defun trim-right-subseq (suffix sequence)
+  "Trim `suffix' from right.
+;; (trim-right-subseq \"abc\" \"xyzabc\") ;; => xyz"
   (if (alexandria:ends-with-subseq suffix sequence)
       (subseq sequence 0 (- (length sequence) (length suffix)))
       sequence))
 
 (defun pem-p (pem)
-  (let ((lines (split-sequence:split-sequence #\newline pem)))    
+  "predicate func for pem, depending on header and footer"
+  (let ((lines (split-sequence:split-sequence #\newline pem)))
     (and (alexandria:starts-with-subseq "-----BEGIN" (car lines))
 	 (alexandria:starts-with-subseq "-----END" (car (last lines))))))
 
 (defun pem-type-p (pem type)
+  "Predicate func for OpenSSL PEM, depending on header and footer"
   (let ((header (car (split-sequence:split-sequence #\newline pem)))
 	(type-header (cond ((eq type 'rsa-private)
 			    "-----BEGIN RSA PRIVATE KEY-----")
@@ -61,13 +80,15 @@
     (string= header type-header)))
 
 (defun rsa-private-pem-p (pem)
+  "Predicate rsa private key pem"
   (pem-type-p pem 'rsa-private))
 
 (defun rsa-public-pem-p (pem)
+  "Predicate rsa public key pem"
   (pem-type-p pem 'public))
 
-;; TODO: if private, work. but public doesn't. need fix.
 (defun extract-pem (pem)
+  "Extract PEM content"
   (if (pem-p pem)
       (let* ((lines (split-sequence:split-sequence #\newline pem))
 	     (header (car lines))
@@ -76,7 +97,7 @@
 	(format nil "~{~A~}" pem-payload))))
 
 (defun parse-der (bytes)
-  "WARN: only RSA private key now"
+  "Parse DER from byte sequence to ASP.1 Object"
   (labels ((inner-parse-field (acc bytes)
 	     (multiple-value-bind (val rest) (get-value bytes)
 	       (if rest
@@ -86,14 +107,16 @@
 
 @export
 (defun parse-pem (pem)
-  (let ((pem-base64 (extract-pem (string-trim '(#\newline) pem))))
-    (if pem-base64
-	(cl-base64:base64-string-to-usb8-array pem-base64))))
+  "Parse PEM to RSA key"
+  (let ((asp1-obj (parse-der (coerce (cl-base64:base64-string-to-usb8-array
+				      (extract-pem (string-trim '(#\newline) pem)))
+				     'list))))
+    (cond ((rsa-private-pem-p pem) (asp1-rsa-private (car asp1-obj)))
+	  ((rsa-public-pem-p pem) (asp1-rsa-public (car asp1-obj)))
+	  (t asp1-obj))))
 
 @export
 (defun read-pem (filepath)
+  "Read PEM file and parse it"
   (with-open-file (in filepath)
-    (let* ((pem (read-string in))
-	   (der (coerce (parse-pem pem) 'list))
-	   (asp1-data (parse-der der)))
-      asp1-data)))
+    (parse-pem (read-string in))))
