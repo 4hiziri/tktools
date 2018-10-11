a301f6a33099106188112ca957d256c9663e9288
tkgsy <meirvg@gmail.com>
2018-10-11 20:28:03 +0900

    Remove unused code

--- crypto/rsa/cl-hack-rsa/src/hackrsa.lisp
+++ crypto/rsa/cl-hack-rsa/src/hackrsa.lisp
@@ -21,7 +21,7 @@ bx' + ay' - (truncate a b) * b y' = gcd(a, b)
 ay' + b(x' - (truncate a b)y') = gcd(a, b)
 compare ax + by = gcd(a, b),
 x = y', y = x' - (truncato a b)y'
-"
+" 
   (if (= b 0)
       (list a 1 0)
       (let* ((egcd (extend-gcd b (mod a b)))
@@ -33,16 +33,16 @@ x = y', y = x' - (truncato a b)y'
 @export
 (defun mod-expt (base exp modulus)
   "more effective expotential and modulus.
-calculate mod at every step of exp."
-  (assert (integerp exp))
+calculate mod at every step of exp."    
+  (assert (integerp exp))  
   (labels ((inner-mod-expt (base exp modulus)
-	     (if (< exp 0) ;; if exp < 0, cannot calc mod so simply return base^exp
-		 (expt base exp)
-		 (loop for acc = 1 then (if (evenp e) acc (mod (* acc b) modulus))
-		       for b = (mod base modulus) then (if (evenp e) (mod (expt b 2) modulus) b)
-		       for e = exp then (if (evenp e) (/ e 2) (1- e))
-		       when (= e 0)
-			 do (return acc)))))
+	     (loop for acc = 1 then (if (evenp e) acc (mod (* acc b) modulus))
+		   for b = (mod base modulus) then (if (evenp e)
+						       (mod (expt b 2) modulus)
+						       b)
+		   for e = exp then (if (evenp e) (/ e 2) (1- e))
+		   when (= e 0)
+		     do (return acc))))
     (if (>= exp 0)
 	(inner-mod-expt base exp modulus)
 	(/ 1 (inner-mod-expt base (- exp) modulus)))))
